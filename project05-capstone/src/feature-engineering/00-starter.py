ae6e0fefbca2433890f24667395ad8a1046ad864
elvyna <vrc.vyna@gmail.com>
2022-01-03 20:47:25 +1300

    Remove unused import

--- project05-capstone/src/feature-engineering/00-starter.py
+++ project05-capstone/src/feature-engineering/00-starter.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
 
 ## use one hot encoder for categorical values
 from sklearn.preprocessing import OneHotEncoder
