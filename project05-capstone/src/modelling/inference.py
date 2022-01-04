e9e3058c880d5c9facb06f201453969e94aadfed
elvyna <vrc.vyna@gmail.com>
2022-01-05 10:31:49 +1300

    Add preprocess_input()

--- project05-capstone/src/modelling/inference.py
+++ project05-capstone/src/modelling/inference.py
@@ -3,6 +3,8 @@ import logging
 import sys
 import os
 from sklearn.ensemble import RandomForestClassifier
+import pandas as pd
+import numpy as np
 import pickle
 
 logging.basicConfig(
@@ -15,6 +17,126 @@ log = logging.getLogger(__name__)
 log.addHandler(logging.StreamHandler(sys.stdout))
 
 JSON_CONTENT_TYPE = "application/json"
+
+
+def determine_time_of_day(df: pd.DataFrame, column_name: str) -> pd.DataFrame:
+    """
+    Determine time of day based on hour of the day.
+    Time of day: early morning, morning, afternoon, or evening.
+
+    :param df: input dataframe to be processed
+    :type df: pd.DataFrame
+    :param column_name: column that stores hour of day
+    :type column_name: str
+    :return: input dataframe with additional time_of_day column
+    :rtype: pd.DataFrame
+    """
+    condition_list = [
+        ((df[column_name] >= 0) & (df[column_name] < 6)),
+        ((df[column_name] >= 6) & (df[column_name] < 12)),
+        ((df[column_name] >= 12) & (df[column_name] < 18)),
+        ((df[column_name] >= 18) & (df[column_name] < 25)),
+    ]
+
+    choice_list = ["early_morning", "morning", "afternoon", "evening"]
+
+    df["time_of_day"] = np.select(condition_list, choice_list, default="unknown")
+    return df
+
+
+def preprocess_input(request_body):
+    ## convert input json object as a dataframe of one row
+    df_test = pd.json_normalize(request_body)
+    ## convert '?' into -99
+    ## reformat data types
+    for col in df_test.columns:
+        ## replace missing value with a numeric value, e.g., -99
+        mask = df_test[col] == "?"
+        df_test.loc[mask, col] = -99
+
+    ## reformat data types
+    # then, convert to numeric
+    numeric_column_list = [
+        "startHour",
+        "startWeekday",
+        "duration",
+        "cCount",
+        "cMinPrice",
+        "cMaxPrice",
+        "cSumPrice",
+        "bCount",
+        "bMinPrice",
+        "bMaxPrice",
+        "bSumPrice",
+        "bStep",
+        "customerNo",
+        "maxVal",
+        "customerScore",
+        "accountLifetime",
+        "payments",
+        "age",
+        "address",
+        "lastOrder",
+    ]
+
+    for col in numeric_column_list:
+        df_test[col] = df_test[col].astype(float)
+
+    condition_list = [
+        (df_test["onlineStatus"] == "y"),
+        (df_test["onlineStatus"] == "n"),
+    ]
+    choice_list = [1, 0]
+    df_test["onlineStatus"] = np.select(
+        condition_list, choice_list, default=df_test["onlineStatus"]
+    )
+
+    df_test["address"] = df_test["address"].astype(int)
+
+    ## remove ID columns
+    df_test.drop(labels=["sessionNo", "customerNo"], axis=1, inplace=True)
+
+    ## determine time of day
+    df_test = determine_time_of_day(df=df_test, column_name="startHour")
+
+    ## one hot encoding
+    selected_feature_list = ["availability", "address", "time_of_day", "onlineStatus"]
+    for selected_feature in selected_feature_list:
+        df_dummy_values = pd.get_dummies(df_test[selected_feature])
+        df_dummy_values.columns = [
+            selected_feature.replace(" ", "_") + "_" + str(col)
+            for col in df_dummy_values.columns
+        ]
+        df_test = pd.concat([df_test, df_dummy_values], axis=1)
+        ## remove redundant features - we've done one hot encoding
+        df_test.drop(labels=selected_feature, axis=1, inplace=True)
+
+    ## reformat
+    categorical_feature_list = [
+        "availability_-99",
+        "availability_completely_not_determinable",
+        "availability_completely_not_orderable",
+        "availability_completely_orderable",
+        "availability_mainly_not_determinable",
+        "availability_mainly_not_orderable",
+        "availability_mainly_orderable",
+        "address_-99",
+        "address_1",
+        "address_2",
+        "time_of_day_afternoon",
+        "time_of_day_early_morning",
+        "time_of_day_evening",
+        "onlineStatus_-99",
+        "onlineStatus_0",
+    ]
+
+    for col in categorical_feature_list:
+        try:
+            df_test[col] = df_test[col].astype(int)
+        except:
+            df_test[col] = 0
+
+    return df_test
 
 
 def model_fn(model_dir):
@@ -44,26 +166,45 @@ def input_fn(request_body, content_type=JSON_CONTENT_TYPE):
     request = json.loads(request_body)
     log.debug(f"Loaded JSON object: {request}")
 
-    ## TO DO
-    ## put data transformation logic in inference.py
-    ## - convert '?' into -99
-    ## reformat data types
-    ## determine time of day
-    ## one hot encoding
-
-    # url = request['url']
-    # img_content = requests.get(url).content
-    # return Image.open(io.BytesIO(img_content))
+    df_test = preprocess_input(request_body=request)
+    return df_test
 
 
 # inference
 def predict_fn(input_object, model):
     log.info("In predict_fn")
-    ## TO DO: transform input
-    log.info("Transforming input")
-    # input_object = test_transform(input_object)
 
     log.info("Calling model")
-    prediction = model(input_object.unsqueeze(0))
+    prediction = model(input_object)
 
     return prediction
+
+
+# if __name__ == "__main__":
+#     request_body = {
+#         "sessionNo": 101,
+#         "startHour": 4,
+#         "startWeekday": 7,
+#         "duration": 0,
+#         "cCount": 2,
+#         "cMinPrice": 30,
+#         "cMaxPrice": 40,
+#         "cSumPrice": 70,
+#         "bCount": 1,
+#         "bMinPrice": 30,
+#         "bMaxPrice": 30,
+#         "bSumPrice": 30,
+#         "bStep": "?",
+#         "onlineStatus": "?",
+#         "availability": "?",
+#         "customerNo": 39,
+#         "maxVal": 200,
+#         "customerScore": 65,
+#         "accountLifetime": 30,
+#         "payments": 2,
+#         "age": 39,
+#         "address": 1,
+#         "lastOrder": 30,
+#     }
+
+#     breakpoint()
